ec2ebd2e6b0e7be6d8500ad80638767ddec264f9
abeer282 <abeerala282@gmail.com>
2020-06-05 19:46:49 +0300

    Add question model

---
+++ TestingSysApi/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSysApi.Models
+{
+    public class Question
+    {
+        public long Id { get; set; }
+
+        public string question { get; set; }
+
+        public string answer1 { get; set; }
+
+        public string answer2 { get; set; }
+
+        public string answer3 { get; set; }
+
+        public string answer4 { get; set; }
+
+        public Int32 true_answer { get; set; }
+    }
+}
