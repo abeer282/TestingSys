c321c2b5512996da0f1d59330c9f25af749e0424
abeer282 <abeerala282@gmail.com>
2020-06-05 19:47:42 +0300

    Add question context

---
+++ TestingSysApi/Contexts/QuestionContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestingSysApi.Models;
+
+namespace TestingSysApi.Contexts
+{
+    public class QuestionContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder
+            .UseSqlite(@"Data Source = TestingSys.db;");
+        }
+
+        public DbSet<Question> Question { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>().HasData(
+            new Question() { Id = 1, question = "What's the capital of France?", answer1 = "Paris", answer2 = "Rome", answer3 = "London", answer4 = "Berlin", true_answer = 1 },
+            new Question() { Id = 2, question = "What's the capital of Italy?", answer1 = "Paris", answer2 = "Rome", answer3 = "London", answer4 = "Berlin", true_answer = 2 }
+            );
+        }
+    }
+}
