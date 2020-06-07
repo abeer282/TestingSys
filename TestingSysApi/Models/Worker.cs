435cacc30aabaecfa877adb7aee3e8e3ca4f350b
abeer282 <abeerala282@gmail.com>
2020-06-07 23:25:45 +0300

    Add worker model

---
+++ TestingSysApi/Models/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSysApi.Models
+{
+    public class Worker
+    {
+        public long Id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public bool did_test { get; set; }
+        public bool pass_test { get; set; }
+
+    }
+}
