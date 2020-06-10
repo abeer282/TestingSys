95c97ba8f59f20a29482db6e125cd857a7fd6978
abeer282 <abeerala282@gmail.com>
2020-06-10 23:02:47 +0300

    Add worker context

---
+++ TestingSysApi/Contexts/WorkerContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestingSysApi.Models;
+
+namespace TestingSysApi.Contexts
+{
+    public class WorkerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder
+            .UseSqlite(@"Data Source = TestingSys.db;");
+        }
+
+        public DbSet<Worker> Worker { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Worker>().HasData(
+                new Worker { Id = 6, fname="Bob", lname="Green", email="bob@green.com" , did_test=false, pass_test=false }
+            );
+        }
+    }
+}
